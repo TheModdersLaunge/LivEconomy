91c7fc7668793056f8f510cf43e014e83ad38bd7
RetronEletron <Bartekjelly@gmail.com>
2023-07-09 17:22:58 +0100

    Implemented a changePassword command into Accounts file.

--- src/main/java/modderslaunge/liveconomy/commands/AccountCommand.java
+++ src/main/java/modderslaunge/liveconomy/commands/AccountCommand.java
@@ -22,8 +22,52 @@ public class AccountCommand {
                 .then(Commands.literal("remove")
                         .then(Commands.argument("name", StringArgumentType.word()).executes(AccountCommand::remove))));
 
+        ctx.register(Commands.literal("account").then(Commands.literal("changepassword")
+                        .then(Commands.argument("name",StringArgumentType.word())
+                .then(Commands.argument("old",StringArgumentType.word())
+                        .then(Commands.argument("new",StringArgumentType.word()).executes(AccountCommand::changepassword))))));
+
     }
 
+    private static int changepassword(CommandContext<CommandSourceStack> ctx) {
+        ServerPlayer user = ctx.getSource().getPlayer();
+        // Check if sender is null.
+        if (user == null) {
+            ModLogger.ctxError("Command has to be sent by a player",ctx);
+            return 0;
+        }
+
+        AccountApi api = new AccountApi(ctx.getSource().getServer());
+
+        // Checks if Account exists
+
+        if (api.getAccount(ctx.getArgument("name",String.class)) == null) {
+            ModLogger.playerError(String.format("Account %s does not exists!",ctx.getArgument("name",String.class)),ctx.getSource().getPlayer());
+            return 0;
+        }
+
+        // Checks if old password matches at all with the account
+        // If it doesn't but they have OP then they can change it no matter what
+
+        if (!api.getAccount(ctx.getArgument("name",String.class)).getPassword()
+                .equals(ctx.getArgument("old",String.class)) && !ctx.getSource().hasPermission(4)) {
+            ModLogger.playerError("Old password is incorrect!",ctx.getSource().getPlayer());
+            return 0;
+        }
+
+        // Checks if new password equals to the old one
+        if (api.getAccount(ctx.getArgument("name",String.class)).getPassword()
+                .equals(ctx.getArgument("new",String.class))) {
+            ModLogger.playerError("Cannot change password to the previous one!",ctx.getSource().getPlayer());
+            return 0;
+        }
+
+        // Changes password
+        new Account(ctx.getArgument("name",String.class),ctx.getArgument("new",String.class),ctx.getSource().getPlayer().getUUID());
+        ModLogger.playerInfo("Password has been successfully reset!",ctx.getSource().getPlayer());
+
+        return 0;
+    }
 
 
     private static int remove(CommandContext<CommandSourceStack> ctx) {
